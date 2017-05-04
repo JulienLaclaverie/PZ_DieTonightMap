d58f980317889702a34386c1213dd7f8139dabc8
Julien <julien.laclaverie@gmail.com>
2017-05-04 12:21:52 +0200

    Exporting the map and creating the base mod
    Export map lots
    Create Mod folder

---
+++ DieTonightMod/media/maps/DieTonightMod/objects.lua
@@ -0,0 +1,92 @@
+objects = {
+  { name = "", type = "SpawnPoint", x = 142, y = 111, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 131, y = 101, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 122, y = 101, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 128, y = 91, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 130, y = 109, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 135, y = 109, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 122, y = 122, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 121, y = 135, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 125, y = 141, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 124, y = 131, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 133, y = 146, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 133, y = 156, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 122, y = 151, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 161, y = 154, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 162, y = 150, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 163, y = 139, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 161, y = 129, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 140, y = 91, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  }
+}
