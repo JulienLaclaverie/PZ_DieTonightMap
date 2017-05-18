40f94ec8e1e0ca7a1b14dda14c0e5a173ce9be26
Sylvain Marty <sylvain.marty@aol.fr>
2017-05-18 18:29:37 +0200

    [Modes] mise en place du mode Un jou = 1/4 d'heure (WIP)

---
+++ mod/DieTonight/media/lua/server/Modes/OneDayInAQuarter.lua
@@ -0,0 +1,31 @@
+-- ================================================
+--           PZ Die Tonight Map & mod              
+--           File created by Sylvain               
+--           Date: 18/05/2017                         
+--           Time: 16:19                         
+-- ================================================
+
+OneDayInAQuarter = {
+    modeName = "OneDayInAQuarter"
+};
+
+-- ServerOptions
+
+OneDayInAQuarter.Tick = function()
+    print("[DT-INFO] OneDayInAQuarter: isClient() --> "..tostring(isClient())..", isServer() --> "..tostring(isServer())..", getSandboxOptions() --> "..tostring(getSandboxOptions()));
+    -- if isServer() then
+        -- print("[DT-INFO] OneDayInAQuarter: Mode matcher enabled on server. activated custom mode --> "..tostring(ServerOptions:getOption("DietonightMode")));
+        -- see SandboxVars in client/LastStand/AReallyCDDAy.lua
+    -- else
+        local serverOptions = ServerOptions:new();
+        print("[DT-INFO] OneDayInAQuarter: Mode matcher enabled on server. activated custom mode --> "..tostring(serverOptions:getOptionByName("DietonightMode")));
+        for i=1,serverOptions:getNumOptions() do
+            print("[DT-INFO] OneDayInAQuarter: "..tostring(serverOptions:getOptionByIndex(i-1):getName()).." --> "..tostring(serverOptions:getOptionByIndex(i-1)));
+        end
+        -- serverOptions:getOptionByName("DietonightMode")
+        -- DietonightMode=OneDayInAQuarter
+    -- end
+end
+
+-- Events.EveryTenMinutes.Add(OneDayInAQuarter.Tick);
+-- Events.EveryDays.Add(OneDayInAQuarter.Tick);
