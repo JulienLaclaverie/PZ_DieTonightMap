0c9d63bb71d5cfc4eb7b555cb5b506faf156c1f0
Sylvain Marty <sylvain.marty@aol.fr>
2017-05-31 19:43:07 +0200

    [Server Management] Close serveur after N days survived
    Working on the issue #17

--- mod/DieTonight/media/lua/server/ServerSettings/ServerSessionManager.lua
+++ mod/DieTonight/media/lua/server/ServerSettings/ServerSessionManager.lua
@@ -7,12 +7,18 @@
 
 ServerSessionManager = {
 
+    currentServerName = "",
+
     currentSession = nil,
+
+    currentSessionSettings = nil,
+
+    optionsAlreadyReloaded = false,
 
     specialSessions = {
         {
             name = "onedayasaquarter",
-            closeSubscriptionAfter = 1
+            closeSubscriptionAfter = 2
         }
     }
 
@@ -20,25 +26,37 @@ ServerSessionManager = {
 
 ServerSessionManager.UpdateServerAccess = function()
     if isServer() then
-        local daysSurvived = getGameTime():getDaysSurvived();
-        local option = getServerOptions():getOptionByName("Open");
+        local daysSurvived = getGameTime():getDay();
+        local option = ServerSessionManager.currentSessionSettings:getServerOptions():getOptionByName("Open");
+        -- print("[DT-INFO] ServerSesssionManager: Day survived="..tostring(daysSurvived).."/"..ServerSessionManager.currentSession.closeSubscriptionAfter..", isOpen="..tostring(option:getValue()));
         if option:getValue() == true and daysSurvived >= ServerSessionManager.currentSession.closeSubscriptionAfter then
             print("[DT-INFO] ServerSesssionManager: Closing subscriptions...");
-            option:setValue(false);
+            ServerSessionManager.updateOpenOption("false");
         end
     end
 end
 
+ServerSessionManager.updateOpenOption = function(value)
+    print("[DT-INFO] Expecting Open="..value)
+    ServerSessionManager.currentSessionSettings:getServerOptions():getOptionByName("Open"):parse(value);
+    ServerSessionManager.currentSessionSettings:saveFiles();
+    print("[DT-INFO] After saving Open="..tostring(ServerSessionManager.currentSessionSettings:getServerOptions():getOptionByName("Open"):getValue()));
+end
+
 ServerSessionManager.PrepareSession = function()
     if isServer() then
-        local serverName = getServerName();
+        ServerSessionManager.currentServerName = getServerName();
+        getGameTime():getModData()["DT_currentServerName"] = ServerSessionManager.currentServerName;
         for i,session in ipairs(ServerSessionManager.specialSessions) do
-            if string.find(string.lower(serverName), session.name) then
+            if string.find(string.lower(ServerSessionManager.currentServerName), session.name) then
                 ServerSessionManager.currentSession = session;
                 if session.closeSubscriptionAfter or session.closeSubscriptionAfter == 0 then
                     print("[DT-INFO] ServerSessionManager: Server will close subs after "..session.closeSubscriptionAfter.." days survived");
+                    ServerSessionManager.currentSessionSettings = ServerSettings.new(ServerSessionManager.currentServerName);
+                    ServerSessionManager.currentSessionSettings:loadFiles();
+
                     -- Activating Open option when server start
-                    getServerOptions():getOptionByName("Open"):setValue(true);
+                    ServerSessionManager.updateOpenOption("true");
                     -- EveryHours or EveryDays | EveryTenMinutes
                     Events.EveryHours.Add(ServerSessionManager.UpdateServerAccess);
                 end
@@ -46,6 +64,41 @@ ServerSessionManager.PrepareSession = function()
             end
         end
     end
+
 end
 
+-- Reload the options one time if necessary
+ServerSessionManager.ReloadOptions = function()
+    local daysSurvived = getGameTime():getDay();
+    local option = getServerOptions():getOptionByName("Open");
+
+    if daysSurvived >= ServerSessionManager.currentSession.closeSubscriptionAfter and
+       not ServerSessionManager.optionsAlreadyReloaded then
+        print("[DT-INFO] Reloading options. survivedDays="..getGameTime():getDay()..", isOpen="..tostring(option:getValue())..", serverName="..tostring(ServerSessionManager.currentServerName))
+        SendCommandToServer("/reloadoptions");
+        ServerSessionManager.optionsAlreadyReloaded = true;
+    end
+
+end
+
+-- Prepare the client custom session
+ServerSessionManager.PrepareClientSession = function()
+    ServerSessionManager.currentServerName = getGameTime():getModData()["DT_currentServerName"];
+
+    if isClient() and getWorld():getGameMode() == "Multiplayer" and ServerSessionManager.currentServerName then
+        for i,session in ipairs(ServerSessionManager.specialSessions) do
+            if string.find(string.lower(ServerSessionManager.currentServerName), session.name) then
+                ServerSessionManager.currentSession = session;
+                if session.closeSubscriptionAfter or session.closeSubscriptionAfter == 0 then
+                    print("[DT-INFO] Adding reload options trigger to client");
+                    Events.EveryHours.Add(ServerSessionManager.ReloadOptions);
+                end
+                break;
+            end
+        end
+    end
+
+end
+
+Events.OnGameStart.Add(ServerSessionManager.PrepareClientSession);
 Events.OnServerStarted.Add(ServerSessionManager.PrepareSession);
