6d0c05729424ebe456905c6da26573172c429b53
Sylvain Marty <sylvain.marty@aol.fr>
2017-07-19 11:26:47 +0200

    Fixing few Z axes in spawnpoints

--- mod/DieTonight/media/maps/The World Beyond/spawnpoints.lua
+++ mod/DieTonight/media/maps/The World Beyond/spawnpoints.lua
@@ -4,49 +4,49 @@ return {
     { worldX = 26, worldY = 21, posX = 161, posY = 154, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 164, posY = 150, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 123, posY = 128, posZ = 0 },
-    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 5 }
+    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 0 }
   },
   fireofficer = {
     { worldX = 26, worldY = 21, posX = 161, posY = 154, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 164, posY = 150, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 123, posY = 128, posZ = 0 },
-    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 5 }
+    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 0 }
   },
   parkranger = {
     { worldX = 26, worldY = 21, posX = 161, posY = 154, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 164, posY = 150, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 123, posY = 128, posZ = 0 },
-    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 5 }
+    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 0 }
   },
   policeofficer = {
     { worldX = 26, worldY = 21, posX = 161, posY = 154, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 164, posY = 150, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 123, posY = 128, posZ = 0 },
-    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 5 }
+    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 0 }
   },
   securityguard = {
     { worldX = 26, worldY = 21, posX = 161, posY = 154, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 164, posY = 150, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 123, posY = 128, posZ = 0 },
-    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 5 }
+    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 0 }
   },
   unemployed = {
     { worldX = 26, worldY = 21, posX = 161, posY = 154, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 164, posY = 150, posZ = 0 },
     { worldX = 26, worldY = 21, posX = 123, posY = 128, posZ = 0 },
-    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 5 },
-    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 5 }
+    { worldX = 26, worldY = 21, posX = 139, posY = 146, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 140, posY = 120, posZ = 0 },
+    { worldX = 26, worldY = 21, posX = 144, posY = 117, posZ = 0 }
   }
 }
 end
